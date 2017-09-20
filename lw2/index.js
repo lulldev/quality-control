9cabe27013fc2e9ab14f20ff75903c6163c7b309
lulldev <lulldev@yandex.ru>
2017-09-20 12:56:56 +0300

    add domain check, links counters, [in progress]

--- lw2/index.js
+++ lw2/index.js
@@ -4,51 +4,79 @@ const validator = require('validator');
 const commandLineArgs = require('command-line-args');
 const fs = require('fs');
 
+function extractHostname(url) {
+    var hostname;
+    if (url.indexOf("://") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+    hostname = hostname.split(':')[0];
+    hostname = hostname.split('?')[0];
+    return hostname;
+}
+
+
 const optionDefinitions = [
-  { name: 'url', alias: 'U', type: String}
+  { name: 'url', alias: 'U', type: String }
 ]
 const options = commandLineArgs(optionDefinitions);
 if (!options.hasOwnProperty('url')) {
-  console.log('URL parametr is empty!');
+  console.log('URL ссылка не указана');
   process.exit(1);
 }
 
 let targetUrl = options.url;
+let targetDomain = extractHostname(targetUrl);
 if (!validator.isURL(targetUrl)) {
-  console.log('URL link is not valide');
+  console.log('URL ссылка указана неверно');
   process.exit(2);
 }
 
+let brokenLinksCounter = 0;
+let allLinksCounter = 0;
 const fAllLinks = fs.createWriteStream('all-links.txt');
 const fBrokenLinks = fs.createWriteStream('broken-links.txt');
 let testedLinks = [];
 
 const parseAndTestLinks = (nextLink) => {
-  request({ uri: nextLink, method: 'GET', encoding: 'binary' },
-    function (err, res, page) {
-      if (res && res.statusCode == '200') {
-        const $ = cheerio.load(page);
-        let links = $('a');
-        $(links).each(function(i, link){
-          const parsedLink = $(link).attr('href');
-          if (parsedLink && validator.isURL(parsedLink) && testedLinks.indexOf(parsedLink) === -1 &&
-              parsedLink.indexOf(targetUrl) > -1) {
-            fAllLinks.write(`${parsedLink} ${res.statusCode}\n`);
-            testedLinks.push(parsedLink);
-            parseAndTestLinks(parsedLink);
-          }
-        });
-      } else {
-        let code;
-        if (typeof res !== 'object') {
-          code = '500';
-        } else {
-          code = res.statusCode;
+  request({ uri: nextLink, method: 'GET', encoding: 'binary' }, (err, res, page) => {
+    if (res && res.statusCode == '200') {
+      const $ = cheerio.load(page);
+      let links = $('a');
+      $(links).each(function (i, link) {
+        const parsedLink = $(link).attr('href');
+        if (parsedLink && validator.isURL(parsedLink) && testedLinks.indexOf(parsedLink) === -1 &&
+          parsedLink.indexOf(targetDomain) > -1) {
+          fAllLinks.write(`${parsedLink} ${res.statusCode}\n`);
+          testedLinks.push(parsedLink);
+          allLinksCounter++;
+          parseAndTestLinks(parsedLink);
         }
-        fBrokenLinks.write(`${nextLink} ${code}\n`);
-        testedLinks.push(nextLink);
+      });
+    } else {
+      let code;
+      if (typeof res !== 'object') {
+        code = '500';
+      } else {
+        code = res.statusCode;
       }
-    });
-}; 
+      fBrokenLinks.write(`${nextLink} ${code}\n`);
+      fAllLinks.write(`${nextLink} ${code}\n`);
+      brokenLinksCounter++;
+      allLinksCounter++;
+      testedLinks.push(nextLink);
+    }
+  });
+};
 
 parseAndTestLinks(targetUrl);
+
+// todo: async output
+const datetime = new Date().toISOString().
+  replace(/T/, ' ').
+  replace(/\..+/, '');
+
+fAllLinks.write(`Всего ссылок: ${allLinksCounter}\n Дата и время: ${datetime}`);
+fBrokenLinks.write(`Всего ссылок: ${brokenLinksCounter}\n Дата и время: ${datetime}`);
