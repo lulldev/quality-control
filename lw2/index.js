b18097774e71f0ea019db830a126e409bf9e56c6
lulldev <lulldev@yandex.ru>
2017-09-20 03:09:09 +0300

    creating request and simple parsing all URL's from primary page

--- lw2/index.js
+++ lw2/index.js
@@ -18,11 +18,14 @@ if (!validator.isURL(targetUrl)) {
   process.exit(2);
 }
 
-// request({ uri: 'http://www.amazon.com/', method: 'GET', encoding: 'binary' },
-//   function (err, res, page) {
-//     //Передаём страницу в cheerio
-//     var $ = cheerio.load(page);
-//     //Идём по DOM-дереву обычными CSS-селекторами
-//     img_src = $('div.s9a3 > div > div > a > div > div > img').attr("src");
-//     console.log(img_src);
-//   });
+request({ uri: targetUrl, method: 'GET', encoding: 'binary' },
+  function (err, res, page) {
+    const $ = cheerio.load(page);
+    let links = $('a');
+    $(links).each(function(i, link){
+      parsedLink = $(link).attr('href');
+      if (validator.isURL(parsedLink)) {
+        console.log(parsedLink);
+      }
+    });
+  });
