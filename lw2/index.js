3e5f9f0061d2e89a213e1fe645592f3278c7739b
lulldev <lulldev@yandex.ru>
2017-10-04 12:55:50 +0300

    new, unstable version on tmp arrays

--- lw2/index.js
+++ lw2/index.js
@@ -5,16 +5,16 @@ const commandLineArgs = require('command-line-args');
 const fs = require('fs');
 
 let extractHostname = (url) => {
-    var hostname;
-    if (url.indexOf("://") > -1) {
-        hostname = url.split('/')[2];
-    }
-    else {
-        hostname = url.split('/')[0];
-    }
-    hostname = hostname.split(':')[0];
-    hostname = hostname.split('?')[0];
-    return hostname;
+  var hostname;
+  if (url.indexOf("://") > -1) {
+    hostname = url.split('/')[2];
+  }
+  else {
+    hostname = url.split('/')[0];
+  }
+  hostname = hostname.split(':')[0];
+  hostname = hostname.split('?')[0];
+  return hostname;
 }
 
 const optionDefinitions = [
@@ -37,22 +37,31 @@ let brokenLinksCounter = 0;
 let allLinksCounter = 0;
 const fAllLinks = fs.createWriteStream('all-links.txt');
 const fBrokenLinks = fs.createWriteStream('broken-links.txt');
-let testedLinks = [];
+let allLinks = [];
+let brokenLinks = [];
 
-const validateLink = (parsedLink, targetDomain) => {
-  let validatedLink = { isValid: false, parsedLink: parsedLink, validatedLink: '' };
-  
-  if (!parsedLink) return validatedLink;
-  
-  if (parsedLink[0] === '/') {
-    validatedLink.isValid = true;
-    validatedLink.validatedLink = `http://${targetDomain}`;
-  } 
-  else if (parsedLink[0] === 'h' && validator.isURL(parsedLink) && parsedLink.indexOf(targetDomain) > -1) {
-    validatedLink.isValid = true;
-    validatedLink.validatedLink = validatedLink.parsedLink;
+const isValidLink = (link) => {
+  return (link.indexOf('javascript') === -1);
+};
+
+const isOwnLink = (link, targetDomain) => {
+  return (validator.isURL(link) && link.indexOf(targetDomain) > -1);
+};
+
+const prepareLink = (link, targetDomain) => {
+  if (!validator.isURL(link) || link.indexOf('http') !== 0) {
+    return `http://${targetDomain}/${link}`
   }
-  return validatedLink;
+  return link;
+};
+
+const issetLink = (link, allLinks) => {
+  allLinks.forEach((linkData) => {
+    if (link === linkData.link) {
+      return true;
+    }
+  });
+  return false;
 };
 
 const parseAndTestLinks = (nextLink) => {
@@ -61,13 +70,12 @@ const parseAndTestLinks = (nextLink) => {
       const $ = cheerio.load(page);
       let links = $('a');
       $(links).each(function (i, link) {
-        const parsedLink = $(link).attr('href');   
-        const validatedLink = validateLink(parsedLink, targetDomain)     
-        if (validatedLink.isValid && testedLinks.indexOf(parsedLink) === -1) {
-          fAllLinks.write(`${parsedLink} ${res.statusCode}\n`);
-          testedLinks.push(parsedLink);
-          allLinksCounter++;
+        let parsedLink = $(link).attr('href');
+        parsedLink = prepareLink(parsedLink, targetDomain);
+        allLinksCounter++;
+        if (isValidLink(parsedLink) && isOwnLink(parsedLink, targetDomain) && !issetLink(parsedLink, allLinks)) {
           parseAndTestLinks(parsedLink);
+          allLinks.push({ link: parsedLink, status: res.statusCode });
         }
       });
     } else {
@@ -77,21 +85,28 @@ const parseAndTestLinks = (nextLink) => {
       } else {
         code = res.statusCode;
       }
-      fBrokenLinks.write(`${nextLink} ${code}\n`);
-      fAllLinks.write(`${nextLink} ${code}\n`);
-      brokenLinksCounter++;
-      allLinksCounter++;
-      testedLinks.push(nextLink);
+      if (!issetLink(nextLink, allLinks)) {
+        brokenLinks.push({ link: nextLink, status: code });
+        allLinks.push({ link: nextLink, status: code });
+      }
     }
   });
-}; 
+};
 
 parseAndTestLinks(targetUrl);
 
 process.on('exit', (code) => {
+
+  let allLinksClear = allLinks.sort((a, b) => { return a.link < b.link ? -1 : 1; }).reduce((allLinks, el) => {
+    if (!allLinks.length || allLinks[allLinks.length - 1].link != el.link) {
+      allLinks.push(el);
+    }
+    return allLinks;
+  }, []);
+
   const datetime = new Date().toISOString().
     replace(/T/, ' ').
     replace(/\..+/, '');
-  fAllLinks.write(`Всего ссылок: ${allLinksCounter}\nДата и время: ${datetime}`);
-  fBrokenLinks.write(`Всего ссылок: ${brokenLinksCounter}\nДата и время: ${datetime}`);
+  fAllLinks.write(`Всего ссылок: ${allLinksClear.length}\nДата и время: ${datetime}`);
+  // fBrokenLinks.write(`Всего ссылок: ${brokenLinksCounter}\nДата и время: ${datetime}`);
 });
